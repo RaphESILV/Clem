d1e32b6612e3dbd2b9967826f46b79f611b99c5a
RaphESILV <148538930+RaphESILV@users.noreply.github.com>
2023-12-12 09:44:18 +0000

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Ajoutez ce script si vous souhaitez faire fonctionner le menu déroulant */
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
